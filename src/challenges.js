d418d322dd233fb95eb5e0f14ae466f765cbe6b3
Victor Murilo <victor2706.vm@gmail.com>
2022-04-12 17:10:37 -0300

    Cumpri o requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -53,15 +53,18 @@ function highestCount(numbers) {
 // console.log(highestCount([-2, -2, -1]))
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
-  if (cat1 < cat2) {
-    return 'cat1'
-  } else if (cat2 < cat1){
-    return 'cat2'
-  } else {
+  let gato1 = Math.abs(cat1 - mouse)
+  let gato2 = Math.abs(cat2 - mouse)
+
+  if (gato1 == gato2 && gato2 == gato1) {
     return 'os gatos trombam e o rato foge'
+  } else if (gato2 < gato1){
+    return 'cat2'
+  } else if (gato1 < gato2) {
+    return 'cat1'
   }
 }
-
+console.log(catAndMouse(1, 0, 2))
 // Desafio 8
 function fizzBuzz(numbers) {
   let coisas = []
