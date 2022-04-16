0bd1e7dbfee5d57403961e34a1fa05cf6f7e27f5
Victor Murilo <victor2706.vm@gmail.com>
2022-04-16 10:20:42 -0300

    Cumpri o requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,11 +1,22 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  if (phoneNumber.length == 11) {
+
+  } else {
+    return "Array com tamanho incorreto."
+  }
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if(lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineB) {
+    if (Math.abs(lineA > lineA - lineB - lineC && lineB > lineA - lineB - lineC && lineC > lineA - lineB - lineC)) {
+      return true
+    }
+    return false
+  } else {
+    return false
+  }
 }
 
 // Desafio 13
